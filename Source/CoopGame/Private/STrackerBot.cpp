dfc240da1d410a7eb9d7971553d92a3647d1d3e3
peterpanpoo-PC\peterpanpoo <giang151093@yahoo.com.vn>
2018-08-17 11:15:17 +0200

    Pickup - Creating pickup class

--- Source/CoopGame/Private/STrackerBot.cpp
+++ Source/CoopGame/Private/STrackerBot.cpp
@@ -199,6 +199,7 @@ void ASTrackerBot::Tick(float DeltaTime)
 
 void ASTrackerBot::NotifyActorBeginOverlap(AActor* OtherActor)
 {
+	Super::NotifyActorBeginOverlap(OtherActor);
 	if (!bStartedSelfDestruction && !bExploded)
 	{
 		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
